41ff0bfe5935545d6cbba9b084638ad8a00cda3e
Davi Rodrigues Felix de Freitas <132140189+davrff@users.noreply.github.com>
2025-10-21 19:09:36 +0000

    Adicionando comentarios

--- xadrez.c
+++ xadrez.c
@@ -2,11 +2,19 @@
 
 int main() {
 
+    // Declrando as variaveis que define quantas casas cada peça vai se mover.
+
     int casasTorre = 5;
     int casasBispo = 5;
     int casasRainha = 8;
 
+    // Variavel de controle 
+
     int i;
+
+    // MOVIMENTO DA TORRE
+    // A Torre se move em linha reta e vertical
+    // Simulamos o movimento dela 5 casas a direita
 
     printf("Movimento da Torre:\n");
     for (i = 1; i <= casasTorre; i++)
@@ -14,11 +22,19 @@ int main() {
         printf("Direita\n");
     }
 
+    // MOVIMENTO DO BISPO
+    // O Bispo se move apenas nas diagonais
+    // Cada casa pecorrida combina duas direçoes, cima e direita
+
     printf("\nMovimento do Bispo:\n");
     for (i = 1; i <= casasBispo; i++)
     {
         printf("Cima, Direita\n");
     }
+
+    // MOVIMENTO DA RAINHA
+    // A Rainha pode se mover em todas direções
+    // Simulamos o movimentos dela 8 casas para a esquerda
 
     printf("\nMovimento da Rainha\n");
     for (i = 1; i <= casasRainha; i++)
@@ -26,6 +42,6 @@ int main() {
         printf("Esquerda\n");
     }
     
-    
+    // FIM DO PROGRAMA
     return 0;
 }
